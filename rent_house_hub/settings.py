6e53881ca76ecdaec35ec0d2a9e28a9b1697bd2c
Gabiro Vladimir Brenn <103499307+gabbrenn@users.noreply.github.com>
2024-12-22 13:18:17 +0200

    Update settings.py

--- rent_house_hub/settings.py
+++ rent_house_hub/settings.py
@@ -125,12 +125,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
